0cd667519a87ae401bbefddb053bc410f1edea4a
Martin Kuehne <martinkuehne@live.de>
2021-06-01 12:42:55 +0200

    setup done for the applications controller

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,10 +1,40 @@
 class ArtworksController < ApplicationController
-  def index
-  end
+  before_action :set_artwork, only: [:show, :destroy]
 
-  def new
+  def index
+    @artworks = Artworks.all
   end
 
   def show
+  end
+
+  def new
+    @artwork = Artwork.new
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+    @artwork.user = current_user
+
+    if @artwork.save
+      render :show
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @artwork.destroy
+    redirect_to artworks_path
+  end
+
+  private
+
+  def set_artwork
+    @artwork = Artwork.find(params[:id])
+  end
+
+  def artwork_params
+    params.require(:restaurant).permit(:title, :description, :version, :privacy, :request)
   end
 end
