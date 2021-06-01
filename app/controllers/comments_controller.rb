6f6f558566312e1d688dfccb7015d87f88876a23
Laura Hauertmann <laurahauertmann@gmail.com>
2021-06-01 12:56:13 +0200

    comment controller added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :artwork_id, :x_offset, :y_offset)
+  end
 end
