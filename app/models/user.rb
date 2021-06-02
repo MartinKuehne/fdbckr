ed368271a883b227ea41f3ef4fa9db1dbcd5dda3
Laura Hauertmann <laurahauertmann@gmail.com>
2021-06-02 16:20:55 +0200

    avatar link trial

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :feedback_requests
   has_many :friendships_as_asker, class_name: "Friendship", foreign_key: :asker_id
   has_many :friendships_as_receiver, class_name: "Friendship", foreign_key: :receiver_id
+  has_one_attached :photo
 
   validates :username, presence: true
   validates :username, uniqueness: true
