a2130e99ad4e4036aa9d6cb06bb7bb9245c3c4ee
Kevin Myung <81194502+kmyung91@users.noreply.github.com>
2021-06-07 15:33:50 +0200

    Update image_grid.js

--- app/javascript/packs/image_grid.js
+++ app/javascript/packs/image_grid.js
@@ -2,13 +2,13 @@ import Masonry from 'masonry-layout';
 
 const initImageGrid = ()=>{
   const masonElement = document.querySelector(".grid")
- new Masonry( masonElement, {
-  // set itemSelector so .grid-sizer is not used in layout
-  itemSelector: '.grid-item',
-  // use element for option
-  gutter: '.gutter-size',
-  percentPosition: true
-})
+  new Masonry( masonElement, {
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.grid-item',
+    // use element for option
+    gutter: '.gutter-size',
+    percentPosition: true
+  })
 }
 
 export {initImageGrid};
